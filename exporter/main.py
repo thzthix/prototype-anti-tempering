87cce5384371e4ddea6cfccd532fd63cb17a7c88
thzthix <seohas0428@naver.com>
2025-07-22 16:06:31 +0900

    scrapper에서 db에 쓰고 exporter에서 읽을 수 있는지 확인

--- exporter/main.py
+++ exporter/main.py
@@ -1,14 +1,33 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from sqlmodel import create_engine, Session, SQLModel
+from models.Company import Company
+from sqlmodel import select
+from contextlib import contextmanager
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable not set.")
 
 engine = create_engine(DATABASE_URL, echo = True) # SQL 쿼리 출력
+@contextmanager
+def get_session():
+    session = Session(engine)
+    try:
+        yield session
+    finally:
+        session.close()
 def main():
     print("Exporter 모듈 시작")
-def get_session():
-    with Session(engine) as session:
-        yield session
+    with get_session() as session:
+        companies = session.exec(select(Company)).all()
+        for company in companies:
+            print(f"ID: {company.id}, Name: {company.name}, Active: {company.is_active}")
+
+import time
+
 if __name__ == "__main__":
     main()
+    print("컨테이너가 종료되지 않도록 대기 중...")
+    while True:
+        time.sleep(60)
--- scraper/main.py
+++ scraper/main.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from sqlmodel import create_engine, SQLModel, Session
 from models.Company import Company
 from models.LoginWebPage import LoginWebPage
+from contextlib import contextmanager
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable not set.")
@@ -16,16 +17,29 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
     print("Database tables created.")
 
+@contextmanager
 def get_session():
-    with Session(engine) as session:
+    session = Session(engine)
+    try:
         yield session
+    finally:
+        session.close()
 
 def main():
     create_db_and_tables()
     print("Scraper service started. Performing scraping and saving data...")
+    with get_session() as session:
+        company = Company(name="교보문고", url="https://mmbr.kyobobook.co.kr/login", is_active=True)
+        session.add(company)
+        session.commit()
+        print("db에 저장되었습니다.")
 
+
+import time
 
 if __name__ == "__main__":
-    print("-----------------------------DATABASE URL: ",DATABASE_URL)
     main()
+    print("컨테이너가 종료되지 않도록 대기 중...")
+    while True:
+        time.sleep(60)
     
